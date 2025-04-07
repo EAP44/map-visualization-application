58d51f8b63b0d59915b5d0740fb8bf350339ac6c
Ayoub <118635029+EAP44@users.noreply.github.com>
2025-04-07 15:08:42 +0100

    Update classrooms.js

--- backend/routes/classrooms.js
+++ backend/routes/classrooms.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const classroomsController = require("../controllers/classroomsController");
-// home route
 router.get("/", classroomsController.getClassrooms);
 
 router.post("/shortest-path", classroomsController.getShortestPath);
